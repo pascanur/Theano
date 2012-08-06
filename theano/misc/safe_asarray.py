2e58824bbc0d773bd2397de577130cbe18c2ee90
Razvan Pascanu <pascanur@iro>
2012-08-06 10:54:40 -0400

    not sure why needed

--- theano/misc/safe_asarray.py
+++ theano/misc/safe_asarray.py
@@ -30,7 +30,7 @@ def _asarray(a, dtype, order=None):
     if str(dtype) == 'floatX':
         dtype = theano.config.floatX
     dtype = numpy.dtype(dtype)  # Convert into dtype object.
-    rval = numpy.asarray(a, dtype=dtype, order=order)
+    rval = numpy.asarray(numpy.asarray(a), dtype=dtype, order=order)
     # Note that dtype comparison must be done by comparing their `num`
     # attribute. One cannot assume that two identical data types are pointers
     # towards the same object (e.g. under Windows this appears not to be the
