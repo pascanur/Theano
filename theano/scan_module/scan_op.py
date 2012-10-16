fe23339d3aa2d05ee166d9e271051376950ed60b
Razvan Pascanu <r.pascanu@gmail.com>
2012-10-16 19:35:33 -0400

    pep8 fixes

--- theano/scan_module/scan_op.py
+++ theano/scan_module/scan_op.py
@@ -292,11 +292,8 @@ class Scan(PureOp):
                                            str(outer_mitmot),
                                            argoffset + idx,
                                            outer_mitmot.type.dtype,
-                                           outer_mitmot.type.ndim,
                                            str(inner_mitmot[ipos + k]),
-                                           inner_mitmot[ipos +
-                                                        k].type.dtype,
-                                           inner_mitmot[ipos + k].type.ndim))
+                                           inner_mitmot[ipos + k].type.dtype))
             ipos += len(itaps)
             for k in xrange(len(otaps)):
                 if (inner_mitmot_outs[opos + k].type.dtype != \
